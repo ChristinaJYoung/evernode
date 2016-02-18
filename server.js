089280d6bb1b88d6aed72b2bf748e4b5fd975fba
Christina <Christina@Billys-MacBook-Pro.local>
2016-02-18 09:52:24 -0600

    create new express server

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict'
+
+
+const express = require('express');
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Server is Running');
+});
+
+app.listen(3000, () => {
+console.log(`Evernode server running on port: ${port}`);
+});
