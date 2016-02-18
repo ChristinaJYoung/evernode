67a0ddea899774745486cead420fcaa33a3a921b
Christina <Christina@Billys-MacBook-Pro.local>
2016-02-18 14:02:02 -0600

    added delete function

--- server.js
+++ server.js
@@ -1,52 +1,32 @@
-'use strict'
+'use strict';
 
 const bodyParser = require('body-parser');
 const express = require('express');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
+
+const note = require('./routes/note');
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-const Note = mongoose.model('Notes', mongoose.Schema({
-  title: String,
-  text: String
-}));
 
 app.set('view engine', 'jade');
 
 app.use(bodyParser.urlencoded({
   extended: false
 }));
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
-  res.send('Server is Running');
+  res.send('Server Running');
 });
 
-app.get('/notes/new', (req, res) => {
-  res.render('new-note');
-});
-
-app.get('/notes/:id', (req, res) => {
-  Note.findById(req.params.id, (err, note) => {
-    if (err) throw err;
-
-    res.render('show-note', {note: note});
-  })
-});
-
-app.post('/notes', (req, res) => {
-  Note.create(req.body, (err, note) => {
-    if (err) throw err;
-    console.log(note);
-  res.redirect(`/notes/${note._id}`);
-  });
-});
+app.use(note);
 
 mongoose.connect('mongodb://localhost:27017/evernode', (err) => {
   if (err) throw err;
 
-app.listen(port, () => {
-console.log(`Evernode server running on port: ${port}`);
+  app.listen(port, () => {
+    console.log(`Evernode server running on port: ${port}`);
+  });
 });
-})
-
