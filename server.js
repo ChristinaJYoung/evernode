58269b3d2cffbb2d8cdc169aa3c8f3c5186d49ae
Christina <Christina@Billys-MacBook-Pro.local>
2016-02-18 10:35:08 -0600

    new note form

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 'use strict'
 
-
+const bodyParser = require('body-parser');
 const express = require('express');
 
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set('view engine', 'jade');
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
 app.get('/', (req, res) => {
   res.send('Server is Running');
+});
+
+app.get('/note/new', (req, res) => {
+  res.render('new-note');
+});
+
+app.post('/notes', (req, res) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.listen(3000, () => {
